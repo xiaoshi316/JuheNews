d1aaa02c6922d94f74689e39de10dcdb80ab7c7f
lisa <onlyloveyd@gmail.com>
2016-12-06 13:36:24 +0800

    add welcomeactivity and drawerlayout

--- app/src/main/java/jzfp/gs/com/juhenews/activity/HomeActivity.java
+++ app/src/main/java/jzfp/gs/com/juhenews/activity/HomeActivity.java
@@ -1,8 +1,13 @@
 package jzfp.gs.com.juhenews.activity;
 
+import android.content.res.ColorStateList;
+import android.content.res.Resources;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
 import android.support.v4.view.PagerTitleStrip;
 import android.support.v4.view.ViewPager;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.TypedValue;
@@ -13,11 +18,14 @@ import android.view.MenuItem;
 import jzfp.gs.com.juhenews.R;
 import jzfp.gs.com.juhenews.adapter.MainPagerAdapter;
 
-public class HomeActivity extends AppCompatActivity {
+public class HomeActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     private Toolbar toolbar = null;
     private ViewPager viewPager = null;
     private MainPagerAdapter mainPagerAdapter = null;
     private PagerTitleStrip pagerTitleStrip = null;
+    private NavigationView navigationView = null;
+    private DrawerLayout drawerLayout = null;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,14 +35,15 @@ public class HomeActivity extends AppCompatActivity {
         //getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
         //透明导航栏
         //getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
+        drawerLayout = (DrawerLayout) findViewById(R.id.dl_main);
+        navigationView = (NavigationView) findViewById(R.id.nv_left);
+        navigationView.setNavigationItemSelectedListener(this);
 
         mainPagerAdapter = new MainPagerAdapter(getSupportFragmentManager());
-
-
         viewPager = (ViewPager) findViewById(R.id.vp_content);
         viewPager.setAdapter(mainPagerAdapter);
 
-        pagerTitleStrip = (PagerTitleStrip)findViewById(R.id.pts);
+        pagerTitleStrip = (PagerTitleStrip) findViewById(R.id.pts);
         pagerTitleStrip.setTextSize(TypedValue.COMPLEX_UNIT_SP, 15);
         pagerTitleStrip.setTextColor(getResources().getColor(R.color.colorWhite));
         pagerTitleStrip.setGravity(Gravity.CENTER);
@@ -52,4 +61,24 @@ public class HomeActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        if (drawerLayout.isDrawerOpen(Gravity.LEFT)) {
+            drawerLayout.closeDrawers();
+        }
+        switch (item.getItemId()) {
+            case R.id.news:
+                viewPager.setCurrentItem(0);
+                break;
+            case R.id.joke:
+                viewPager.setCurrentItem(1);
+                break;
+            case R.id.history:
+                viewPager.setCurrentItem(2);
+                break;
+            default:
+                break;
+        }
+        return true;
+    }
 }
--- app/src/main/java/jzfp/gs/com/juhenews/activity/WebActivity.java
+++ app/src/main/java/jzfp/gs/com/juhenews/activity/WebActivity.java
@@ -6,12 +6,15 @@ import android.support.v4.app.TaskStackBuilder;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.webkit.WebView;
 
 import jzfp.gs.com.juhenews.R;
 
 public class WebActivity extends AppCompatActivity {
+
+    private String URL = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,21 +23,28 @@ public class WebActivity extends AppCompatActivity {
 
         Intent intent = getIntent();
         Bundle bundle = intent.getExtras();
-        String URL = bundle.getBundle("extra").getString("URL");
+        URL = bundle.getBundle("extra").getString("URL");
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
         toolbar.setNavigationIcon(R.drawable.back);
         toolbar.setTitleTextAppearance(this, R.style.ToolBarTextAppearance);
 
-        WebView webView = (WebView)findViewById(R.id.wv_content);
+        WebView webView = (WebView) findViewById(R.id.wv_content);
         webView.loadUrl(URL);
 
     }
 
     @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.toolbar, menu);
+        return true;
+    }
+
+    @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
-            case android.R.id.home:
+            case android.R.id.home: {
                 Intent upIntent = NavUtils.getParentActivityIntent(this);
                 if (NavUtils.shouldUpRecreateTask(this, upIntent)) {
                     TaskStackBuilder.create(this)
@@ -44,7 +54,17 @@ public class WebActivity extends AppCompatActivity {
                     upIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
                     NavUtils.navigateUpTo(this, upIntent);
                 }
-                break;
+            }
+            break;
+            case R.id.share:
+            {
+                Intent intent = new Intent();
+                intent.setAction(Intent.ACTION_SEND);
+                intent.setType("text/plain");
+                intent.putExtra(Intent.EXTRA_TEXT, URL);
+                startActivity(Intent.createChooser(intent, getTitle()));
+            }
+            break;
             default:
                 break;
         }
